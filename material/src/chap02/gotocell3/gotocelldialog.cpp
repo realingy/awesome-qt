3bd0abd31e8e075082d3387a709d57cb0b832c5e
realingy <754037927@qq.com>
2020-04-06 14:22:19 +0800

    元对象机制和uic机制

--- material/src/chap02/gotocell3/gotocelldialog.cpp
+++ material/src/chap02/gotocell3/gotocelldialog.cpp
@@ -8,8 +8,8 @@ GoToCellDialog::GoToCellDialog(QWidget *parent)
     setupUi(this);
     buttonBox->button(QDialogButtonBox::Ok)->setEnabled(false);
 
-    QRegExp regExp("[A-Za-z][1-9][0-9]{0,2}");
-    lineEdit->setValidator(new QRegExpValidator(regExp, this));
+    QRegExp regExp("[A-Za-z][1-9][0-9]{0,2}"); //正则表达式
+    lineEdit->setValidator(new QRegExpValidator(regExp, this)); //设置正则表达式
 
     connect(buttonBox, SIGNAL(accepted()), SLOT(accept()));
     connect(buttonBox, SIGNAL(rejected()), SLOT(reject()));
@@ -17,6 +17,7 @@ GoToCellDialog::GoToCellDialog(QWidget *parent)
 
 void GoToCellDialog::on_lineEdit_textChanged(const QString &str)
 {
+    Q_UNUSED(str)
     //buttonBox->button(QDialogButtonBox::Ok)->setEnabled(lineEdit->hasAcceptableInput());
     buttonBox->button(QDialogButtonBox::Ok)->setEnabled(true);
 }
