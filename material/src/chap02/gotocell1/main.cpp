900ad31280468b979c614e653b5ca6c3da719a86
realingy <754037927@qq.com>
2020-04-06 15:17:29 +0800

    去除无效代码

--- material/src/chap02/gotocell1/main.cpp
+++ material/src/chap02/gotocell1/main.cpp
@@ -3,18 +3,8 @@
 
 #include "ui_gotocelldialog.h"
 
-#include <iostream>
-
-int i = 12;
-
 int main(int argc, char *argv[])
 {
-#if 0
-    int i = i;
-    std::cout << "i = " << i << std::endl;
-    return 0;
-#endif
-
 #if 1
     QApplication app(argc, argv);
 
